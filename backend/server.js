1a9956cbe67728389d80aea2119eba47e273e127
Ori <oribennett@gmail.com>
2024-06-24 09:23:10 +0300

    Step5

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//routers
+
+//not found handler
+mongoose
+.connect(process.env.MONGO_CONNECTION_STRING)
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`server running on port ${PORT}`);
+    });
+}).catch((err) => {
+    console.log(err.message);
+});
