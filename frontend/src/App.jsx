fd6108613ef9e0e44dc6fb5e44447bdbc17eb89f
Ori <oribennett@gmail.com>
2024-06-24 13:34:39 +0300

    after break

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,30 @@
 import './App.css'
-import Title from './components/shared/Title.jsx'
+//import Title from './components/shared/Title.jsx'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Container from "react-bootstrap/Container";
+import HomePage from "./pages/HomePage.jsx";
+import Footer from './components/shared/Footer.jsx';
+import Header from './components/shared/Header.jsx';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 
 function App() {
   return (
-    <>
-      <Title title="Home Page"/>
-    </>
+    <BrowserRouter>
+    <div className='d-flex flex-column side-allPage min-width'>
+      {/* {toast component} */}
+      <Header/>
+      <main>
+        <Container className='mt-3'>
+          <Routes>
+            <Route path='/' element={<HomePage/>}/>
+          </Routes>
+        </Container>
+      </main>
+      <Footer/>
+    </div>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import Title from "../components/shared/Title.jsx"
+
+const HomePage = () => {
+    return (
+        <div>
+            <Title title="HomePage"/>
+            HomePage
+        </div>
+    )
+}
+
+export default HomePage;
